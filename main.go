4d0eee1a08d28ce29770cec49897a9d1dc48aa96
jming514 <tang_lester@hotmail.com>
2023-09-17 17:27:00 -0400

    add POST route handling json

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
@@ -25,18 +27,69 @@ func main() {
 	r.Handle("/app", fsHandler)
 	r.Handle("/app/*", fsHandler)
 	apiR.Get("/healthz", healthz)
-	apiR.HandleFunc("/reset", cfg.metrics)
+	apiR.HandleFunc("/reset", cfg.reset)
+	apiR.Post("/validate_chirp", cfg.validateChirp)
 
-	//adminR.Get("/metrics", cfg.metrics)
-	adminR.Get("/metrics", adminFsHandler)
+	adminR.Get("/metrics", cfg.adminFsHandler)
 
 	corsMux := middlewareCors(r)
 	httpServer := &http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
+
 	log.Printf("Server started at %s", httpServer.Addr)
 	log.Fatal(httpServer.ListenAndServe())
+}
+
+func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		log.Printf("Error decoding paramters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	fmt.Printf(strconv.Itoa(len(params.Body)))
+
+	type returnVals struct {
+		Valid bool   `json:"valid"`
+		Error string `json:"error"`
+	}
+
+	respVals := returnVals{}
+
+	if len(params.Body) > 140 {
+		w.WriteHeader(400)
+		respVals.Error = "Chirp is too long"
+	} else {
+		respVals.Valid = true
+	}
+
+	dat, err := json.Marshal(respVals)
+
+	fmt.Printf(string(dat))
+
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("error writing: %s", err)
+		return
+	}
 }
 
 func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
@@ -51,9 +104,20 @@ func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func adminFsHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Set("Content-Type", "text/html")
-	http.FileServer(http.Dir("./admin/metrics/"))
+func (cfg *apiConfig) adminFsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(fmt.Sprintf(`
+<html>
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+</body>
+</html>
+`, cfg.fileserverHits)))
+	if err != nil {
+		return
+	}
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
