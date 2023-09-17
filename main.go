8a53f101dc5d711260a0d5ff62d7ab33290938a5
jming514 <tang_lester@hotmail.com>
2023-09-17 18:53:27 -0400

    add word filtering

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type apiConfig struct {
@@ -14,23 +16,24 @@ type apiConfig struct {
 }
 
 func main() {
-	port := "8080"
-	r := chi.NewRouter()
-	apiR := chi.NewRouter()
-	adminR := chi.NewRouter()
-	r.Mount("/api", apiR)
-	r.Mount("/admin", adminR)
+	const port = "8080"
+	const filepathRoot = "."
 
 	cfg := apiConfig{}
-
-	fsHandler := cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	r := chi.NewRouter()
+	fsHandler := cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	r.Handle("/app", fsHandler)
 	r.Handle("/app/*", fsHandler)
+
+	apiR := chi.NewRouter()
 	apiR.Get("/healthz", healthz)
 	apiR.HandleFunc("/reset", cfg.reset)
 	apiR.Post("/validate_chirp", cfg.validateChirp)
+	r.Mount("/api", apiR)
 
+	adminR := chi.NewRouter()
 	adminR.Get("/metrics", cfg.adminFsHandler)
+	r.Mount("/admin", adminR)
 
 	corsMux := middlewareCors(r)
 	httpServer := &http.Server{
@@ -50,33 +53,40 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
-
 	if err != nil {
-		log.Printf("Error decoding paramters: %s", err)
-		w.WriteHeader(500)
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, 500, "Error decoding parameters...")
 		return
 	}
 
-	fmt.Printf(strconv.Itoa(len(params.Body)))
-
 	type returnVals struct {
-		Valid bool   `json:"valid"`
-		Error string `json:"error"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	respVals := returnVals{}
 
 	if len(params.Body) > 140 {
-		w.WriteHeader(400)
-		respVals.Error = "Chirp is too long"
+		respondWithError(w, 400, "Chirp is too long")
+		return
 	} else {
-		respVals.Valid = true
+		profanity := []string{"kerfuffle", "sharbert", "fornax"}
+
+		res := strings.Split(params.Body, " ")
+		for i, v := range res {
+			for _, c := range profanity {
+				if strings.ToLower(v) == c {
+					res[i] = "****"
+				}
+			}
+		}
+		respVals.CleanedBody = strings.Join(res, " ")
 	}
 
-	dat, err := json.Marshal(respVals)
+	respondWithJSON(w, 200, respVals)
+}
 
-	fmt.Printf(string(dat))
-
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
@@ -84,12 +94,24 @@ func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
 		log.Printf("error writing: %s", err)
 		return
 	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	payload := errorResponse{
+		Error: msg,
+	}
+
+	respondWithJSON(w, code, payload)
 }
 
 func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
@@ -107,14 +129,14 @@ func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
 func (cfg *apiConfig) adminFsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(fmt.Sprintf(`
+	_, err := fmt.Fprintf(w, `
 <html>
 <body>
 	<h1>Welcome, Chirpy Admin</h1>
 	<p>Chirpy has been visited %d times!</p>
 </body>
 </html>
-`, cfg.fileserverHits)))
+`, cfg.fileserverHits)
 	if err != nil {
 		return
 	}
