146eeabf11897acb47d6b03c027bbe76fd8241d5
jming514 <tang_lester@hotmail.com>
2023-09-21 21:13:07 -0400

    finish loadDB function

--- internals/database/db.go
+++ internals/database/db.go
@@ -20,6 +20,7 @@ type Chirp struct {
 	Body string `json:"body"`
 }
 
+// NewDB Create a new database connection
 func NewDB(path string) (*DB, error) {
 	mux := &sync.RWMutex{}
 	database := DB{
@@ -27,20 +28,16 @@ func NewDB(path string) (*DB, error) {
 		mux:  mux,
 	}
 
-	_, err := os.Stat(path)
+	err := database.ensureDB()
+	// TODO: Not sure if I should return nil, err or not
 	if err != nil {
-		// create db
-		file, err := os.Create(path)
-		if err != nil {
-			return nil, err
-		}
-
-		defer file.Close()
+		return nil, err
 	}
 
 	return &database, nil
 }
 
+// CreateChirp creates a new chirp and saves it to disk
 func (db *DB) CreateChirp(body string) (Chirp, error) {
 	db.mux.Lock()
 	file, err := os.OpenFile(db.path, os.O_RDWR|os.O_CREATE, 0755)
@@ -84,4 +81,53 @@ func (db *DB) CreateChirp(body string) (Chirp, error) {
 	defer file.Close()
 	defer db.mux.Unlock()
 	return newChirp, nil
+}
+
+// GetChirps returns all chirps in the database
+func (db *DB) GetChirps() ([]Chirp, error) {
+}
+
+func (db *DB) loadDB() (DBStructure, error) {
+	db.mux.Lock()
+
+	dbStructure := DBStructure{
+		Chirps: map[int]Chirp{},
+	}
+
+	file, err := os.OpenFile(db.path, os.O_RDONLY, 0755)
+	if err != nil {
+		return dbStructure, err
+	}
+	fileData, err := os.ReadFile(db.path)
+	if err != nil {
+		return dbStructure, err
+	}
+
+	err = json.Unmarshal(fileData, &dbStructure)
+	if err != nil {
+		return dbStructure, err
+	}
+
+	err = file.Close()
+	if err != nil {
+		return dbStructure, err
+	}
+
+	defer db.mux.Unlock()
+	return dbStructure, err
+}
+
+func (db *DB) ensureDB() error {
+	_, err := os.Stat(db.path)
+	if err != nil {
+		// create db
+		file, err := os.Create(db.path)
+		if err != nil {
+			return err
+		}
+
+		return file.Close()
+	}
+
+	return nil
 }
